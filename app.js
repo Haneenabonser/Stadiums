aea9b540a1182808cf4bb9fb9e3dc433253d1a5b
Haneenabonser <aabonser@gmail.com>
2021-03-21 14:20:33 +0200

    modified .js file

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var favoriteTeam = prompt("Who is your favorite team?");
 console.log(favoriteTeam);
 document.write('<h4>' + "Oh cool " + userName + "!" + " This is a picture of your favorite team stadium." + '</h4>');
 
+
+while (favoriteTeam!=='real madrid'&&favoriteTeam!=="barcelona"&&favoriteTeam!=="manchester city"&&favoriteTeam!=="juventus"){
+    favoriteTeam=prompt("Please choose only one of these teams: real madrid, barcelona, manchester city, juventus");
+}
+
 if (favoriteTeam === 'real madrid') {
     document.write('<img src="Pictures/realmadrid.jpg"/>'); document.body.style.backgroundColor= "Blue";
 }
@@ -22,3 +27,14 @@ else if (favoriteTeam === 'juventus') {
 }
 else { alert("Sadly, we don't have a picture of your favorite team stadium, choose another team"); 
 }
+
+
+var starRating= prompt("What star rating would you give World Stadiums, 1-5?")
+
+var stars= " ";
+
+for (var i=0;i<starRating;i++) {
+    stars+='<img src="Pictures/star.jpg" width="25px"/>';
+}
+
+document.write(stars);
