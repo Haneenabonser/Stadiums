74e6f86f3bb53754f2f10708a358fa3af828f639
Haneenabonser <aabonser@gmail.com>
2021-03-22 15:41:40 +0200

    added css file

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-var userName = prompt("What's your name?");
-console.log(userName);
-document.write('<h3>' + 'Welcome ' + userName + '</h3>')
+// var userName = prompt("What's your name?");
+// console.log(userName);
+// document.write('<h3>' + 'Welcome ' + userName + '</h3>')
 
-var favoriteTeam = prompt("Who is your favorite team?");
-console.log(favoriteTeam);
-document.write('<h4>' + "Oh cool " + userName + "!" + " This is a picture of your favorite team stadium." + '</h4>');
+// var favoriteTeam = prompt("Who is your favorite team?");
+// console.log(favoriteTeam);
+// document.write('<h4>' + "Oh cool " + userName + "!" + " This is a picture of your favorite team stadium." + '</h4>');
+
+// var getFavoriteTeam= function() {
+//     while (favoriteTeam !== 'real madrid' && favoriteTeam !== "barcelona" && favoriteTeam !== "manchester city" && favoriteTeam !== "juventus") {
+//         favoriteTeam = prompt("Please choose only one of these teams: real madrid, barcelona, manchester city, juventus");
+//     }
+    
+//     if (favoriteTeam === 'real madrid') {
+//         document.write('<img src="Pictures/realmadrid.jpg"/>'); document.body.style.backgroundColor = "Blue";
+//     }
+//     else if
+//         (favoriteTeam === 'barcelona') {
+//         document.write('<img src="Pictures/barcelona.jpg"/>'); document.body.style.backgroundColor = "red";
+//     }
+//     else if
+//         (favoriteTeam === 'manchester city') {
+//         document.write('<img src="Pictures/manchestercity.jpg"/>'); document.body.style.backgroundColor = "babyBlue";
+//     }
+//     else if (favoriteTeam === 'juventus') {
+//         document.write('<img src="Pictures/Juve.jpg"/>'); document.body.style.backgroundColor = "Gray";
+//     }
+// }
+
+// getFavoriteTeam();
 
 
-while (favoriteTeam!=='real madrid'&&favoriteTeam!=="barcelona"&&favoriteTeam!=="manchester city"&&favoriteTeam!=="juventus"){
-    favoriteTeam=prompt("Please choose only one of these teams: real madrid, barcelona, manchester city, juventus");
-}
-
-if (favoriteTeam === 'real madrid') {
-    document.write('<img src="Pictures/realmadrid.jpg"/>'); document.body.style.backgroundColor= "Blue";
-}
-else if
-    (favoriteTeam === 'barcelona') {
-        document.write('<img src="Pictures/barcelona.jpg"/>'); document.body.style.backgroundColor= "red";
-}
-else if
-    (favoriteTeam === 'manchester city') {
-        document.write('<img src="Pictures/manchestercity.jpg"/>'); document.body.style.backgroundColor= "babyBlue";
-}
-else if (favoriteTeam === 'juventus') {
-    document.write('<img src="Pictures/Juve.jpg"/>'); document.body.style.backgroundColor= "Gray";
-}
-else { alert("Sadly, we don't have a picture of your favorite team stadium, choose another team"); 
-}
 
 
-var starRating= prompt("What star rating would you give World Stadiums, 1-5?")
+// var starRating = prompt("What star rating would you give World Stadiums, 1-5?")
+// var getStarRating = function () {
+//     var stars = " ";
 
-var stars= " ";
+//     for (var i = 0; i < starRating; i++) {
+//         stars += '<img src="Pictures/star.jpg" width="25px"/>';
 
-for (var i=0;i<starRating;i++) {
-    stars+='<img src="Pictures/star.jpg" width="25px"/>';
-}
+//     }
 
-document.write(stars);
+//     return stars
+
+// }
+// document.write(getStarRating());
