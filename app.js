5b5509d1b7408c1d4fd74299d34545d30a0fbf78
Haneenabonser <aabonser@gmail.com>
2021-03-22 22:22:09 +0200

    Added functions to js file

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var userName = prompt("What's your name?");
 console.log(userName);
 document.write('<h3>' + 'Welcome ' + userName + '</h3>')
 
+
 var favoriteTeam = prompt("Who is your favorite team?");
-console.log(favoriteTeam);
 document.write('<h4>' + "Oh cool " + userName + "!" + " This is a picture of your favorite team stadium." + '</h4>');
 
+function getFavoriteTeam() {
     while (favoriteTeam !== 'real madrid' && favoriteTeam !== "barcelona" && favoriteTeam !== "manchester city" && favoriteTeam !== "juventus") {
         favoriteTeam = prompt("Please choose only one of these teams: real madrid, barcelona, manchester city, juventus");
     }
-    
+
     if (favoriteTeam === 'real madrid') {
         document.write('<img src="Pictures/realmadrid.jpg"/>');
     }
@@ -19,17 +20,20 @@ document.write('<h4>' + "Oh cool " + userName + "!" + " This is a picture of you
     }
     else if
         (favoriteTeam === 'manchester city') {
-        document.write('<img src="Pictures/manchestercity.jpg"/>'); 
+        document.write('<img src="Pictures/manchestercity.jpg"/>');
     }
     else if (favoriteTeam === 'juventus') {
         document.write('<img src="Pictures/Juve.jpg"/>');
     }
 
+}
+getFavoriteTeam();
+
 
 
 
 var starRating = prompt("What star rating would you give World Stadiums, 1-5?")
-var getStarRating = function() {
+var getStarRating = function () {
     var stars = " ";
 
     for (var i = 0; i < starRating; i++) {
